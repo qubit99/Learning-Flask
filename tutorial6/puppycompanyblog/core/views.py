63b059c0210e13298773b6d39a922f9a5d09610e
qubit99 <architranjan9@gmail.com>
2020-05-03 20:14:03 +0530

    completed blog

--- tutorial6/puppycompanyblog/core/views.py
+++ tutorial6/puppycompanyblog/core/views.py
@@ -1,3 +1,4 @@
+from puppycompanyblog.models import BlogPost
 from flask import render_template, redirect, url_for, request, Blueprint
 
 core = Blueprint('core', __name__)
@@ -5,8 +6,9 @@ core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    # more to come
-    return render_template('index.html')
+    page = request.args.get('page',1,type=int)
+    blog_posts = BlogPost.query.order_by(BlogPost.datetime.desc()).paginate(page = page, per_page = 5)
+    return render_template('index.html', blog_posts = blog_posts)
 
 @core.route('/info')
 def info():
